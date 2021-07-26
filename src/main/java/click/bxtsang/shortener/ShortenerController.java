83e3098dd52740f82ef0ebb82e90df7108bc18a3
bxtsang <bxtsang.2018@sis.smu.edu.sg>
2021-07-26 22:00:56 +0800

    Add logic to handle urls not starting with http:// or https:// or //

--- src/main/java/click/bxtsang/shortener/ShortenerController.java
+++ src/main/java/click/bxtsang/shortener/ShortenerController.java
@@ -17,7 +17,11 @@ public class ShortenerController {
     @Post("/create")
     public String shortenUrl(Map<String, String> body) {
         // can do some error handling here
+        String url = body.get("url");
+        if (!url.substring(0, 4).equals("http") || !url.substring(0, 2).equals("//")) {
+            url = "//" + url;
+        }
 
-        return shortenerService.shortenUrl(body.get("url"));
+        return shortenerService.shortenUrl(url);
     }
 }
